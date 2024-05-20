f7a52d5a980d0f23a6fdbdad103457741e0d9217
Archimedes4 <82121191+AMCanada16@users.noreply.github.com>
2024-05-20 15:25:53 -0500

    made sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './image-qr-code.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div style={{padding: "16px"}}>
+          <img src={logo} className="Image" alt="logo" />
+          <div className='Text-Container'>
+            <p className='Main-P'>
+              Improve your front-end skills by building projects
+            </p>
+            <p className='Second-P'>
+              Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
